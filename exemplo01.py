8d6daab7c067e7241000e62d8296e6154b7c3c55
hmaiajunior <juniorce04@gmail.com>
2024-12-04 21:16:18 -0300

    Primeiro exemplo sobre função

---
+++ exemplo01.py
@@ -0,0 +1,12 @@
+# Função que soma dois arquivos
+
+valor_1 = 3
+valor_2 = 5
+
+def soma(valor_1: float, valor_2: float) -> float:
+    resultado_da_soma = valor_1 + valor_2
+    return resultado_da_soma
+
+valor_3 = soma(valor_1, valor_2)
+
+print(f"o resultado da soma é: {valor_3}")
