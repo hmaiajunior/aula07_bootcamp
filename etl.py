0142ae80f90e77b4d31878f41e8e395b80389a3c
hmaiajunior <juniorce04@gmail.com>
2024-12-08 11:51:34 -0300

    função para filtrar produtos entregues

--- etl.py
+++ etl.py
@@ -15,7 +15,23 @@ def ler_csv(nome_do_arquivo_csv: str) -> list[dict]:
             lista.append(linha)
     return lista
 
-vendas_itens: list[dict]
+# vendas_itens: list[dict]
 
-vendas_itens = ler_csv(path_arquivo)
-print(vendas_itens)
+# vendas_itens = ler_csv(path_arquivo)
+# print(vendas_itens)
+
+def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
+
+    """
+    Função que filtra produtos onde entrega = True
+
+    """
+    lista_com_produtos_filtrados = []
+    for produto in lista:
+        if produto.get("entregue") == "True":
+            lista_com_produtos_filtrados.append(produto)
+    return lista_com_produtos_filtrados
+
+lista_de_produtos = ler_csv(path_arquivo)
+produtos_entregues = filtrar_produtos_nao_entregues(lista_de_produtos)
+print(produtos_entregues)
